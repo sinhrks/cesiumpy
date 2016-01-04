5393e625afa5ccd2a2f4e90d4e071d90478758c0
sinhrks <sinhrks@gmail.com>
2016-01-04 20:46:41 +0900

    REL: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PACKAGE = 'cesiumpy'
 README = 'README.rst'
 REQUIREMENTS = 'requirements.txt'
 
-VERSION = '0.1.0'
+VERSION = '0.1.1.dev'
 
 def read(fname):
   # file must be read as utf-8 in py3 to avoid to be bytes
