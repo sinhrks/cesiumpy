8fdc95df2dbfe257423a244f5eebcbf9debb0ee1
sinhrks <sinhrks@gmail.com>
2016-01-17 17:25:07 +0900

    ENH: Support temporary image material

--- cesiumpy/entities/tests/test_material.py
+++ cesiumpy/entities/tests/test_material.py
@@ -4,9 +4,11 @@
 import nose
 import unittest
 
+import re
 import traitlets
 
 import cesiumpy
+import cesiumpy.testing as tm
 
 
 class TestImageMaterial(unittest.TestCase):
@@ -15,6 +17,20 @@ class TestImageMaterial(unittest.TestCase):
         m = cesiumpy.entities.material.ImageMaterialProperty('xxx.png')
         self.assertEqual(repr(m), 'ImageMaterialProperty(xxx.png)')
         self.assertEqual(m.script, """new Cesium.ImageMaterialProperty({image : "xxx.png"})""")
+
+
+class TestTempImageMaterial(unittest.TestCase):
+
+    def test_matplotlibimage(self):
+        tm._skip_if_no_matplotlib()
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        img = np.random.randint(0, 255, (100, 100, 3))
+        ax = plt.imshow(img)
+        m = cesiumpy.entities.material.TemporaryImageMaterialProperty(ax.figure)
+        self.assertTrue(re.match("""new Cesium\\.ImageMaterialProperty\\({image : "\w+\\.png"}\\)""", m.script))
 
 
 if __name__ == '__main__':
